c3a53664157016e3208d9963ecfcd157cb3c04df
John Hamm <johndhamm1@gmail.com>
2017-05-08 17:26:16 -0500

    revised login: delete teams ref, temp auto login, disable new user register

--- client/app/controllers/loginCtrl.js
+++ client/app/controllers/loginCtrl.js
@@ -11,10 +11,10 @@ app.controller("loginCtrl", function($scope, UserFactory, DbFactory, $location){
 	$scope.showCreateTeam = false;
 	$scope.showRegisterForm = false;
 
-	DbFactory.getTeams()
-		.then((teams) => {
-			$scope.teams = teams;
-		})
+	//*********** temp auto login for development *********
+	$scope.email = 'test@aero.com';
+	$scope.password = 'testpass';
+
 
 	$scope.login = () => {
 		let regUser = false;
@@ -45,77 +45,39 @@ app.controller("loginCtrl", function($scope, UserFactory, DbFactory, $location){
 			})
 	}
 
-	$scope.register = () => {
+	// $scope.register = () => {
 
-		let userExists = false;
-		$scope.user = false;
-		const newUserObj = {
-			email: $scope.regEmail,
-			password: $scope.regPassword,
-			first_name: $scope.first_name,
-			last_name: $scope.last_name,
-			team_id: $scope.team_id
-		}
-		// console.log("newUserObj", newUserObj);
+	// 	let userExists = false;
+	// 	$scope.user = false;
+	// 	const newUserObj = {
+	// 		email: $scope.regEmail,
+	// 		password: $scope.regPassword,
+	// 		first_name: $scope.first_name,
+	// 		last_name: $scope.last_name,
+	// 		team_id: $scope.team_id
+	// 	}
+	// 	// console.log("newUserObj", newUserObj);
 
-		DbFactory.getAllCoaches()
-			.then((coachesArray) => {
-				coachesArray.forEach((coach) => {
-					if (coach.email === newUserObj.email) {
-						userExists = true;
-						$scope.user = true;
-					}
-				})
-			})
-			.then(() => {
-				if (!userExists) {
-					DbFactory.addCoach(newUserObj)
-						.then((coachId) => DbFactory.getCoach(coachId))
-						.then((coach) => {
-							UserFactory.setCurrentCoach(coach[0]);
-							$location.path('/coach');
-						})
-				}
-			})
-	}
+	// 	DbFactory.getAllCoaches()
+	// 		.then((coachesArray) => {
+	// 			coachesArray.forEach((coach) => {
+	// 				if (coach.email === newUserObj.email) {
+	// 					userExists = true;
+	// 					$scope.user = true;
+	// 				}
+	// 			})
+	// 		})
+	// 		.then(() => {
+	// 			if (!userExists) {
+	// 				DbFactory.addCoach(newUserObj)
+	// 					.then((coachId) => DbFactory.getCoach(coachId))
+	// 					.then((coach) => {
+	// 						UserFactory.setCurrentCoach(coach[0]);
+	// 						$location.path('/coach');
+	// 					})
+	// 			}
+	// 		})
+	// }
 
-
-	$scope.joinTeam = () => {
-		$scope.showSelectTeam = true;
-		$scope.showCreateTeam = false;
-		$scope.showRegisterForm = false;
-	}
-
-	$scope.createTeam = () => {
-		$scope.showCreateTeam = true;
-		$scope.showSelectTeam = false;
-		$scope.showRegisterForm = false;
-	}
-
-	$scope.pickTeam = () => {
-		$scope.team_id = $scope.teamSelect;
-		// console.log("team selected", $scope.team_id);
-		DbFactory.getTeamName($scope.team_id)
-			.then((teamName) => {
-				$scope.team_name = teamName[0].team_name;
-				$scope.showRegisterForm = true;
-				$scope.showSelectTeam = false;
-			})
-	}
-
-	$scope.addTeam = () => {
-		$scope.team_name = $scope.newTeam_name;
-		const newTeamObj = { team_name: $scope.newTeam_name};
-		DbFactory.addTeam(newTeamObj)
-			.then((newTeamId) => {
-				$scope.team_id = newTeamId[0];
-				DbFactory.getTeams()
-					.then((teams) => {
-						$scope.teams = teams;
-						$scope.showRegisterForm = true;
-						$scope.showCreateTeam = false;
-					})
-			})
-	}
 
 });
