1450f33b60d09d326891b12d0ade0b1c8d557522
John Hamm <johndhamm1@gmail.com>
2017-05-13 18:51:36 -0500

    locker: hide admin button if coach doesn't have admin privileges

--- client/app/controllers/coachCtrl.js
+++ client/app/controllers/coachCtrl.js
@@ -4,6 +4,7 @@ app.controller("coachCtrl", function($scope, UserFactory, $location){
 
 	const currentCoach = UserFactory.getCurrentCoach();
 	$scope.coach = currentCoach.first_name;
+	$scope.showAdmin = currentCoach.admin;
 
 	if (currentCoach.admin === undefined) {
 		$location.path('/login');
